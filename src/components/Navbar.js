6e058beef341b5776cce49f6b1c06553266d5626
Gagan <gaganjnv2001@gmail.com>
2023-02-10 12:50:23 +0530

    final

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
-import React ,{useState} from 'react'
+import React from 'react'
 import {Link} from "react-router-dom";
 
-import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   const capitalize = (word) => {
